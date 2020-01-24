bd3ee3fc8bf645f2e40cc48e27e190f2b7613cbd
cicloswgit <ciclosw@gmail.com>
2020-01-24 19:25:00 -0300

    implementação do método subtração

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,15 @@
 require_relative "./lib/calculator.rb"
 
 RSpec.describe Calculator do
-    describe '#add' do
-      it 'returns the sum of its arguments' do
-        expect(Calculator.new.add(1, 2)).to eq(3)
-      end
+  describe '#add' do
+    it 'estou escrevendo em português' do
+      expect(Calculator.new.add(1, 2)).to eq(3)
     end
   end
+
+  describe '#sub' do
+    it 'returns the subtraction of its arguments' do
+      expect(Calculator.new.sub(1, 2)).to eq(3)
+    end
+  end
+end
